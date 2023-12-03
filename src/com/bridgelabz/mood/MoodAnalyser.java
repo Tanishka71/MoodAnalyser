e174c02ffac9e5373af25d4543a5066549aa5e07
Tanishka71 <86073425+Tanishka71@users.noreply.github.com>
2023-12-03 14:47:39 +0530

    FINAL COMMIT

--- src/com/bridgelabz/mood/MoodAnalyser.java
+++ src/com/bridgelabz/mood/MoodAnalyser.java
@@ -1,14 +1,16 @@
 package com.bridgelabz.mood;
 
 public class MoodAnalyser {
-    String message;
+
+
+	private String message;
     
     /*
 	 * @desc:default constructor 
 	 * @params:none
 	 * @return:none
 	 */
-    MoodAnalyser(){
+    public MoodAnalyser(){
     	this.message=null;
     }
     
@@ -17,21 +19,40 @@ public class MoodAnalyser {
  	 * @params:String 
  	 * @return:none
  	 */
-    MoodAnalyser(String msg){
+    public MoodAnalyser(String msg){
     	this.message=msg;
     }
 	
+    // @desc:Getter for message field
+    public String getMessage() {
+        return message;
+    }
+
+    // @desc:Setter for message field
+    public void setMessage(String message) {
+        this.message = message;
+    }
 	/*
 	 * @desc:analyse and print whether mood is sad or happy
 	 * @params:String
 	 * @return:String
 	 */
-	String analyseMood() {
-		if(message.contains("Sad")) {
-			return "Sad";
-		}
-		else {
-			return "Happy";
-		}
-	}
+	public String analyseMood()throws MoodAnalysisException {
+		 try{
+	            if (message == null) {
+	                return "Happy"; 
+	            }// Return "Happy" for null mood
+	            else if(message.toLowerCase().contains("happy")){
+	                return "HAPPY";
+	            }else if(message.toLowerCase().contains("sad")){
+	                return "SAD";
+	            }else{
+	                throw new MoodAnalysisException(MoodAnalysisError.INVALID_MOOD);
+	            }
+
+	        }catch(MoodAnalysisException e){
+	            return e.getMessage();
+
+	        }
+	    }
 }
