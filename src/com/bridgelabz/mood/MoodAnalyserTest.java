253e8b1bfe39e6bcf8a6581a68cb29f3d18c0cdc
Tanishka71 <86073425+Tanishka71@users.noreply.github.com>
2023-12-03 14:46:51 +0530

    FINAL COMMIT

--- src/com/bridgelabz/mood/MoodAnalyserTest.java
+++ src/com/bridgelabz/mood/MoodAnalyserTest.java
@@ -21,5 +21,39 @@ class MoodAnalyserTest {
 		assertEquals("Happy",mood.analyseMood(),
 				"Checks for the mood.");
 	}
+	 
+	@Test
+	/* @desc:check for happy mood (TESTCASE 2.1) */
+	void checkForException() throws MoodAnalysisException {
+		MoodAnalyser mood=new MoodAnalyser();
+		assertEquals("Happy",mood.analyseMood(),
+				"Checks for the mood.");
+	}
+	
+    @Test
+    /* @desc:check for null mood (TESTCASE 3.1) */
+   public void testNullMood() throws MoodAnalysisException {
+       String message = null;
+       MoodAnalyser moodAnalysis = new MoodAnalyser(message);
+       MoodAnalysisException exception = assertThrows(MoodAnalysisException.class, () -> {
+           moodAnalysis.analyseMood();
+       });
+       assertEquals(MoodAnalysisError.EMPTY_MOOD, exception.getError());
+   assertEquals("Mood should not be empty or null", exception.getMessage());
+   }
+    
+    @Test
+    /* @desc:check for empty mood (TESTCASE 3.2) */
+    public void testEmptyMood() {
+        String message = "";  // Empty mood
+        MoodAnalyser moodAnalysis = new MoodAnalyser(message);
+
+        MoodAnalysisException exception = assertThrows(MoodAnalysisException.class, () -> {
+            moodAnalysis.analyseMood();
+        });
+
+        assertEquals(MoodAnalysisError.EMPTY_MOOD, exception.getError());
+        assertEquals("Mood should not be empty or null", exception.getMessage());
+    }
 
 }
